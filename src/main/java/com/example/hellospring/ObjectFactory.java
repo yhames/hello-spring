10ce0d08c37b01c48f45336d951f6a23195e0f55
yhames <wjddn494@naver.com>
2024-12-14 13:42:13 +0900

    BeanFactory 추가

--- src/main/java/com/example/hellospring/ObjectFactory.java
+++ src/main/java/com/example/hellospring/ObjectFactory.java
@@ -1,11 +1,17 @@
 package com.example.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
     }

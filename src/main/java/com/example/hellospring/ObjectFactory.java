8e6bae54ef987e283523b73867788f5c16daa5ae
yhames <wjddn494@naver.com>
2024-12-14 13:47:51 +0900

    컴포넌트 스캔

--- src/main/java/com/example/hellospring/ObjectFactory.java
+++ src/main/java/com/example/hellospring/ObjectFactory.java
@@ -1,18 +1,9 @@
 package com.example.hellospring;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
-
-    @Bean
-    public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
-    }
 }

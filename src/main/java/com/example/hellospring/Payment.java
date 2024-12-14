d2fcfbea69c6293d49ad33b9f3b3cd61399f2dfc
yhames <wjddn494@naver.com>
2024-12-14 13:19:23 +0900

    refactoring

--- src/main/java/com/example/hellospring/Payment.java
+++ src/main/java/com/example/hellospring/Payment.java
@@ -3,48 +3,8 @@ package com.example.hellospring;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public class Payment {
-
-    private Long orderId;
-    private String currency;
-    private BigDecimal foreignCurrencyAmount;
-    private BigDecimal exRate;
-    private BigDecimal convertedAmount;
-    private LocalDateTime validUntil;
-
-    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate,
-                   BigDecimal convertedAmount, LocalDateTime validUntil) {
-        this.orderId = orderId;
-        this.currency = currency;
-        this.foreignCurrencyAmount = foreignCurrencyAmount;
-        this.exRate = exRate;
-        this.convertedAmount = convertedAmount;
-        this.validUntil = validUntil;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public BigDecimal getForeignCurrencyAmount() {
-        return foreignCurrencyAmount;
-    }
-
-    public BigDecimal getExRate() {
-        return exRate;
-    }
-
-    public BigDecimal getConvertedAmount() {
-        return convertedAmount;
-    }
-
-    public LocalDateTime getValidUntil() {
-        return validUntil;
-    }
+public record Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate,
+                      BigDecimal convertedAmount, LocalDateTime validUntil) {
 
     @Override
     public String toString() {

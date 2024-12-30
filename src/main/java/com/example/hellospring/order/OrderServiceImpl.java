ad664dbbb4e080ad97d109678e7ac97d7174796d
yhames <wjddn494@naver.com>
2024-12-30 22:39:52 +0900

    `@Transactional` AOP 추가

--- src/main/java/com/example/hellospring/order/OrderServiceImpl.java
+++ src/main/java/com/example/hellospring/order/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.hellospring.order;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.beans.Transient;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -15,6 +17,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public Order createOrder(String no, BigDecimal total) {
         Order order = new Order(no, total);
         orderRepository.save(order);
@@ -22,6 +25,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public List<Order> createOrders(List<OrderReq> reqs) {
         return reqs.stream()
                 .map(req -> createOrder(req.no(), req.total()))
